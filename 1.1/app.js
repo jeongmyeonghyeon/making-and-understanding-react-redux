ad7c7c0c8d3f2a5161881e34cdcfc7af13de7590
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-09-06 19:44:12 +0900

    1.1 변동 용이성 (2) - 하나의 변경사항이 createElement 전체의 테스트사항이 되지 않도록 각 create 로직을 함수로 분리

--- 1.1/app.js
+++ 1.1/app.js
@@ -1,20 +1,25 @@
+function createH1(props) {
+  return [document.createElement("h1")].map((element) => {
+    Object.entries({ ...props, "data-id": "title" }).forEach(([name, value]) =>
+      element.setAttribute(name, value)
+    );
+    return element;
+  })[0];
+}
+function createDiv(props) {
+  return [document.createElement("div")].map((element) => {
+    Object.entries({ ...props, "data-id": "layout" }).forEach(([name, value]) =>
+      element.setAttribute(name, value)
+    );
+    return element;
+  })[0];
+}
+
 function createElement(type, props) {
   switch (type) {
     case "h1":
-      return [document.createElement("h1")].map((element) => {
-        Object.entries({ ...props, "data-id": "title" }).forEach(
-          ([name, value]) => element.setAttribute(name, value)
-        );
-        return element;
-      })[0];
-      break;
+      return createH1(props);
     case "div":
-      return [document.createElement("div")].map((element) => {
-        Object.entries({ ...props, "data-id": "layout" }).forEach(
-          ([name, value]) => element.setAttribute(name, value)
-        );
-        return element;
-      })[0];
-      break;
+      return createDiv(props);
   }
 }
