83ea7590ad948e9d27b023b93b9bebe823d491ab
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-09-14 14:32:58 +0900

    기존 구조 정의방식 주석제거

--- 2.4 createElement/src/app.js
+++ 2.4 createElement/src/app.js
@@ -1,43 +1,4 @@
-import { createDOM, createElement, render } from "./react";
-
-// const vdom = {
-//   tag: "p",
-//   props: {},
-//   children: [
-//     {
-//       tag: "h1",
-//       props: {},
-//       children: ["React 만들기"],
-//     },
-//     {
-//       tag: "ul",
-//       props: {},
-//       children: [
-//         {
-//           tag: "li",
-//           props: {
-//             style: "color:red",
-//           },
-//           children: ["첫 번째 아이템"],
-//         },
-//         {
-//           tag: "li",
-//           props: {
-//             style: "color:blue",
-//           },
-//           children: ["두 번째 아이템"],
-//         },
-//         {
-//           tag: "li",
-//           props: {
-//             style: "color:green",
-//           },
-//           children: ["세 번째 아이템"],
-//         },
-//       ],
-//     },
-//   ],
-// };
+import { createElement, render } from "./react";
 
 const vdom = createElement(
   "p",
