621505e2b8cbea1bc329e8ca1bd763b7cd02c583
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-09-14 19:27:08 +0900

    Part2-5. README.md 내용 추가

--- 2.6 함수 컴포넌트/src/app.js
+++ 2.6 함수 컴포넌트/src/app.js
@@ -2,16 +2,21 @@
 import { createElement, render } from "./react";
 
 function Title(props) {
+  console.log("여기는 Title"); // 함수 컴포넌트가 자바스크립트 함수기 때문에 이렇게 자연스럽게(!) 자바스크립트를 쓸수 있음.
   return <h1>{props.children}</h1>;
+}
+
+function Item(props) {
+  return <li style={`color: ${props.color}`}>{props.children}</li>;
 }
 
 const vdom = (
   <p>
     <Title>React 정말 잘 만들기</Title>
     <ul>
-      <li style="color:red">첫 번째 아이템</li>
-      <li style="color:green">두 번째 아이템</li>
-      <li style="color:blue">세 번째 아이템</li>
+      <Item color="red">첫 번째 아이템 111</Item>
+      <Item color="green">두 번째 아이템 222</Item>
+      <Item color="blue">세 번째 아이템 333</Item>
     </ul>
   </p>
 );
