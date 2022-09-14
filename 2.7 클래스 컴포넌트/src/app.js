a2af75fa2c888bb03f7547b8c58cd11cfe45a335
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-09-14 20:06:04 +0900

    클래스 컴포넌트 확인

--- 2.7 클래스 컴포넌트/src/app.js
+++ 2.7 클래스 컴포넌트/src/app.js
@@ -3,13 +3,8 @@ import { createElement, render, Component } from "./react";
 
 class Title extends Component {
   render() {
-    return <h1>{props.children}</h1>;
+    return <h1>{this.props.children}</h1>;
   }
-}
-
-function Title(props) {
-  console.log("여기는 Title"); // 함수 컴포넌트가 자바스크립트 함수기 때문에 이렇게 자연스럽게(!) 자바스크립트를 쓸수 있음.
-  return <h1>{props.children}</h1>;
 }
 
 function Item(props) {
@@ -18,7 +13,7 @@ function Item(props) {
 
 const App = () => (
   <p>
-    <Title>React 정말 잘 만들기</Title>
+    <Title>React 정말 클래스 컴포넌트 잘 만들기</Title>
     <ul>
       <Item color="red">첫 번째 아이템 111</Item>
       <Item color="green">두 번째 아이템 222</Item>
--- 2.7 클래스 컴포넌트/src/react.js
+++ 2.7 클래스 컴포넌트/src/react.js
@@ -20,17 +20,27 @@ export function createDOM(node) {
   return element;
 }
 
+function makeProps(props, children) {
+  return {
+    ...props,
+    children: children.length === 1 ? children[0] : children,
+  };
+}
+
 export function createElement(tag, props, ...children) {
   props = props || {};
 
   if (typeof tag === "function") {
-    if (children.length > 0) {
-      return tag({
-        ...props,
-        children: children.length === 1 ? children[0] : children,
-      });
+    // 실제 리액트에서는 Symbol을 만들어 처리하고 있음.
+    if (tag.prototype instanceof Component) {
+      const instance = new tag(makeProps(props, children));
+      return instance.render();
     } else {
-      return tag(props);
+      if (children.length > 0) {
+        return tag(makeProps(props, children));
+      } else {
+        return tag(props);
+      }
     }
   } else {
     return { tag, props, children };
