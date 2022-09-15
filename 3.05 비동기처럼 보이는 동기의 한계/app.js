daa9d164eba18afada9f70f16d86960a142f29f1
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-09-15 23:42:31 +0900

    Part3-5. 비동기처럼 보이는 동기의 한계 환경셋팅

---
+++ 3.05 비동기처럼 보이는 동기의 한계/app.js
@@ -0,0 +1,15 @@
+import { createStore } from "./redux.js";
+import { reducer } from "./reducer.js";
+import * as Actions from "./actions.js";
+
+const store = createStore(reducer);
+
+store.subscribe(function () {
+  console.log(store.getState());
+});
+
+store.dispatch(Actions.increase());
+store.dispatch(Actions.increase());
+store.dispatch(Actions.increase());
+store.dispatch(Actions.decrease());
+store.dispatch(Actions.reset());
