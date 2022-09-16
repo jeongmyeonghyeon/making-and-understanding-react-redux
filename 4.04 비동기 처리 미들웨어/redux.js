ca2e10ee0c3adac0d891a96f621c12e2289a33a1
jeongmyeonghyeon <jeongmyeonghyeon@gmail.com>
2022-09-16 10:50:05 +0900

    Part4-4. 비동기 처리기 미들웨어 환경셋팅

---
+++ 4.04 비동기 처리 미들웨어/redux.js
@@ -0,0 +1,39 @@
+export const actionCreator = (type) => (payload) => ({
+  type,
+  payload,
+});
+
+export function createStore(reducer, middlewares = []) {
+  let state;
+  const handlers = [];
+
+  function dispatch(action) {
+    state = reducer(state, action);
+    handlers.forEach((handler) => handler());
+  }
+
+  function getState() {
+    return state;
+  }
+
+  function subscribe(handler) {
+    handlers.push(handler);
+  }
+
+  middlewares = Array.from(middlewares).reverse();
+  let lastDispatch = dispatch;
+
+  const store = {
+    getState,
+    subscribe,
+    dispatch,
+  };
+
+  middlewares.forEach((middleware) => {
+    lastDispatch = middleware(store)(lastDispatch);
+  });
+
+  store.dispatch = lastDispatch;
+
+  return store;
+}
